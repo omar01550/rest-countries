e5e9b878e3f8d1e8967338e9f60ff0526a08c53d
omar medhat <107444038+omar01550@users.noreply.github.com>
2022-11-07 21:08:38 +0200

    Update common.js

--- js/common.js
+++ js/common.js
@@ -46,7 +46,7 @@ themeToggler.addEventListener("click",function (e) {
 // handel language
 let selectLang = document.querySelector(".select-lang");
 window.addEventListener("load",handelLanguage);
-selectLang.addEventListener("select",setLang);
+selectLang.addEventListener("click",setLang);
 function setLang(e) {
      localStorage.setItem("lang",e.target.value);
      handelLanguage()
